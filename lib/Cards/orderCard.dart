b1dfdde9f384980eef4f3a197628898d1b44c85a
datsabahandude <mohd.noor.amlee@gmail.com>
2023-07-05 20:10:41 +0800

    add edit and change default font

--- lib/Cards/orderCard.dart
+++ lib/Cards/orderCard.dart
@@ -1,6 +1,7 @@
 import 'package:car_rental/models/order_display.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:google_fonts/google_fonts.dart';
 // import 'package:intl/intl.dart';
@@ -62,6 +63,8 @@ class OrderCard extends StatelessWidget {
     /// price
     double price = double.parse('${card.price}');
     //
+    final priceEditingController = TextEditingController();
+    final infoEditingController = TextEditingController();
     return Container(
       constraints:
           const BoxConstraints(maxHeight: double.infinity, maxWidth: 550),
@@ -157,7 +160,7 @@ class OrderCard extends StatelessWidget {
                             child: Text(
                               '${card.img}',
                               style: GoogleFonts.manrope(
-                                  fontSize: 18,
+                                  fontSize: 14,
                                   textStyle: const TextStyle(
                                       color: Color(0xFF702c00),
                                       fontWeight: FontWeight.bold)),
@@ -189,6 +192,7 @@ class OrderCard extends StatelessWidget {
                               displayTime,
                               style: GoogleFonts.manrope(
                                   textStyle: TextStyle(
+                                      fontSize: 14,
                                       color: Colors.amber[900],
                                       fontWeight: FontWeight.w600)),
                               overflow: TextOverflow.ellipsis,
@@ -200,6 +204,7 @@ class OrderCard extends StatelessWidget {
                               'RM $price',
                               style: GoogleFonts.manrope(
                                   textStyle: const TextStyle(
+                                      fontSize: 14,
                                       color: Color(0xFF00B406),
                                       fontWeight: FontWeight.w600)),
                               overflow: TextOverflow.ellipsis,
@@ -212,8 +217,8 @@ class OrderCard extends StatelessWidget {
                         '${card.range}',
                         textAlign: TextAlign.center,
                         style: GoogleFonts.manrope(
-                            textStyle:
-                                const TextStyle(fontWeight: FontWeight.w600)),
+                            textStyle: const TextStyle(
+                                fontSize: 12, fontWeight: FontWeight.w600)),
                       ),
                       card.status == 'Pending'
                           ? Text(
@@ -221,6 +226,7 @@ class OrderCard extends StatelessWidget {
                               style: GoogleFonts.manrope(
                                   textStyle: TextStyle(
                                       color: bilaColor,
+                                      fontSize: 14,
                                       fontWeight: FontWeight.w600)),
                             )
                           : Container(),
@@ -265,7 +271,7 @@ class OrderCard extends StatelessWidget {
                                                     style: GoogleFonts.manrope(
                                                         textStyle:
                                                             const TextStyle(
-                                                      fontSize: 20,
+                                                      fontSize: 18,
                                                       color: Color(0xFF00B406),
                                                       fontWeight:
                                                           FontWeight.w500,
@@ -276,7 +282,7 @@ class OrderCard extends StatelessWidget {
                                                     style: GoogleFonts.manrope(
                                                         textStyle:
                                                             const TextStyle(
-                                                      fontSize: 20,
+                                                      fontSize: 18,
                                                       color: Color(0xFF00B406),
                                                       fontWeight:
                                                           FontWeight.w500,
@@ -288,15 +294,161 @@ class OrderCard extends StatelessWidget {
                                 InkWell(
                                   splashColor: const Color(0xff360c72),
                                   onTap: () {
+                                    priceEditingController.text = '$price';
+                                    infoEditingController.text = '${card.info}';
                                     showDialog(
                                         context: context,
                                         builder: ((context) {
                                           return AlertDialog(
+                                            title: Text('Info',
+                                                style: GoogleFonts.manrope()),
                                             shape: RoundedRectangleBorder(
                                                 borderRadius:
-                                                    BorderRadius.circular(24)),
-                                            content: Text('${card.info}',
-                                                style: GoogleFonts.manrope()),
+                                                    BorderRadius.circular(16)),
+                                            content: SingleChildScrollView(
+                                              child: Column(
+                                                children: [
+                                                  TextFormField(
+                                                    minLines: 1,
+                                                    maxLines: 6,
+                                                    autofocus: false,
+                                                    textInputAction:
+                                                        TextInputAction.newline,
+                                                    controller:
+                                                        infoEditingController,
+                                                    onSaved: (value) {
+                                                      priceEditingController
+                                                          .text = value!;
+                                                    },
+                                                    decoration:
+                                                        const InputDecoration(
+                                                      constraints:
+                                                          BoxConstraints(
+                                                              maxWidth: 200),
+                                                      floatingLabelBehavior:
+                                                          FloatingLabelBehavior
+                                                              .never,
+                                                      prefixIcon: Icon(
+                                                          Icons.description,
+                                                          color: Color(
+                                                              0xff360c72)),
+                                                    ),
+                                                  ),
+                                                  const SizedBox(height: 10),
+                                                  TextFormField(
+                                                      maxLength: 7,
+                                                      autofocus: false,
+                                                      controller:
+                                                          priceEditingController,
+                                                      keyboardType:
+                                                          const TextInputType
+                                                                  .numberWithOptions(
+                                                              decimal: true),
+                                                      inputFormatters: <TextInputFormatter>[
+                                                        FilteringTextInputFormatter
+                                                            .allow(RegExp(
+                                                                r'^(\d+)?\.?\d{0,2}'))
+                                                      ],
+                                                      validator: (value) {
+                                                        if (value!.isEmpty) {
+                                                          return 'Price?';
+                                                        }
+                                                        return null;
+                                                      },
+                                                      onSaved: (value) {
+                                                        priceEditingController
+                                                            .text = value!;
+                                                      },
+                                                      decoration:
+                                                          InputDecoration(
+                                                        constraints:
+                                                            const BoxConstraints(
+                                                                maxWidth: 200),
+                                                        floatingLabelBehavior:
+                                                            FloatingLabelBehavior
+                                                                .never,
+                                                        errorStyle:
+                                                            GoogleFonts.manrope(
+                                                          textStyle:
+                                                              const TextStyle(
+                                                            color: Color(
+                                                                0xFFFF1C0C),
+                                                            fontWeight:
+                                                                FontWeight.w500,
+                                                          ),
+                                                        ),
+                                                        fillColor: const Color(
+                                                            0xFFE9E9E9),
+                                                        filled: true,
+                                                        prefixIcon: const Icon(
+                                                            Icons
+                                                                .monetization_on_outlined,
+                                                            color: Color(
+                                                                0xff360c72)),
+                                                        contentPadding:
+                                                            const EdgeInsets
+                                                                    .fromLTRB(
+                                                                20, 15, 20, 15),
+                                                        // contentPadding: const EdgeInsets.symmetric(vertical: 40)
+                                                        hintText: "Price (RM)",
+                                                        hintStyle:
+                                                            GoogleFonts.manrope(
+                                                          textStyle:
+                                                              const TextStyle(
+                                                            fontSize: 16.0,
+                                                            color: Colors.black,
+                                                          ),
+                                                        ),
+                                                        border:
+                                                            InputBorder.none,
+                                                      )),
+                                                ],
+                                              ),
+                                            ),
+                                            actions: [
+                                              Center(
+                                                child: Padding(
+                                                  padding:
+                                                      const EdgeInsets.fromLTRB(
+                                                          0, 5, 0, 5),
+                                                  child: ElevatedButton(
+                                                    onPressed: () {
+                                                      _editInfo(
+                                                          priceEditingController
+                                                              .text,
+                                                          infoEditingController
+                                                              .text);
+                                                      Navigator.push(
+                                                        context,
+                                                        MaterialPageRoute(
+                                                            builder: (context) =>
+                                                                const HomePage()),
+                                                      );
+                                                    },
+                                                    style: ElevatedButton.styleFrom(
+                                                        backgroundColor:
+                                                            Colors.white,
+                                                        shape: RoundedRectangleBorder(
+                                                            borderRadius:
+                                                                BorderRadius
+                                                                    .circular(
+                                                                        12))),
+                                                    clipBehavior:
+                                                        Clip.antiAlias,
+                                                    child: Text(
+                                                      'update info'
+                                                          .toUpperCase(),
+                                                      style: GoogleFonts.manrope(
+                                                          textStyle:
+                                                              const TextStyle(
+                                                                  fontSize: 14,
+                                                                  color: Colors
+                                                                      .black)),
+                                                    ),
+                                                  ),
+                                                ),
+                                              )
+                                            ],
                                           );
                                         }));
                                   },
@@ -313,7 +465,7 @@ class OrderCard extends StatelessWidget {
                                         'Info details',
                                         style: GoogleFonts.manrope(
                                             textStyle: const TextStyle(
-                                          fontSize: 20,
+                                          fontSize: 18,
                                           // color: Color(0xFFC21C1C),
                                           fontWeight: FontWeight.w500,
                                         )),
@@ -345,7 +497,7 @@ class OrderCard extends StatelessWidget {
                                         'Delete',
                                         style: GoogleFonts.manrope(
                                             textStyle: const TextStyle(
-                                          fontSize: 20,
+                                          fontSize: 18,
                                           color: Color(0xFFC21C1C),
                                           fontWeight: FontWeight.w500,
                                         )),
@@ -419,6 +571,32 @@ class OrderCard extends StatelessWidget {
           toastLength: Toast.LENGTH_SHORT,
           gravity: ToastGravity.CENTER,
           timeInSecForIosWeb: 1,
+          backgroundColor: Colors.red,
+          textColor: Colors.white,
+          fontSize: 16.0);
+    }
+  }
+
+  Future _editInfo(price, info) async {
+    try {
+      FirebaseFirestore.instance
+          .collection('Orders')
+          .doc('${card.oid}')
+          .update({'price': price, 'info': info});
+      Fluttertoast.showToast(
+          msg: "Status Changed",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.CENTER,
+          timeInSecForIosWeb: 1,
+          backgroundColor: Colors.green,
+          textColor: Colors.white,
+          fontSize: 16.0);
+    } catch (e) {
+      Fluttertoast.showToast(
+          msg: "Something went wrong!",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.CENTER,
+          timeInSecForIosWeb: 2,
           backgroundColor: Colors.red,
           textColor: Colors.white,
           fontSize: 16.0);
